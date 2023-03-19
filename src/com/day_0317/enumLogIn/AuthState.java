bfb86637c5e916e86e9eae3f02e1cc886418e8c3
minyoung <alsdud11737@gmail.com>
2023-03-19 17:41:02 +0900

    [feat] 0317 enum AuthState class 삭제

--- src/com/day_0317/enumLogIn/AuthState.java
+++
@@ -1,8 +0,0 @@
-package com.day_0317.enumLogIn;
-
-public class AuthState {
-	public static final int AUTHENTICATED = 1;
-	public static final int UNAUTHENTICATED = 2;
-	public static final int AUTHENTICATING = 3;
-
-}
