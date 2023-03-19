7789cb7819e9b00d044c6a6e0f12c529393f1231
minyoung <alsdud11737@gmail.com>
2023-03-19 20:57:00 +0900

    [feat] 0317 collection hashSet 코드 연습

--- src/com/day_0314/Java_collection2.java
+++ src/com/day_0314/Java_collection2.java
@@ -2,17 +2,24 @@ package com.day_0314;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Java_collection2 {
 	public static void main(String[] args) {
-	
-	Set<String> colors = new HashSet<>();
-	colors.add("red");
-	colors.add("green");
-	colors.add("blue");
-	
-	colors.add("red");
-	System.out.println(colors.size());
-		
+
+		Set<String> colors = new HashSet<>();
+		colors.add("red");
+		colors.add("green");
+		colors.add("blue");
+
+		colors.add("red");
+//	System.out.println(colors.size());
+
+		Iterator<String> iter = colors.iterator();
+		while (iter.hasNext()) {
+			String color = iter.next();
+			System.out.println(color);
+		}
+
 	}
 }
