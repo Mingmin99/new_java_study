68799b2f8f559610fd5ba91f758984b5dfff6b78
minyoung <alsdud11737@naver.com>
2023-03-21 17:56:27 +0900

    [feat] 0321 java 응용 8장 network 코드연습

---
+++ src/com/day_0321/java05/network/Main.java
@@ -0,0 +1,16 @@
+package com.day_0321.java05.network;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		java.net.URL 클래스를 사용하여 다음 주소의 그림 파일을 읽어서 PC에 저장하는 프로그램을 작성하시오.
+//		그림 파일 주소 : https://alimipro.com/favicon.ico
+//		저장 위치 : 아무데나
+//		파일명 : icon.ico
+		String imageUrl = "https://alimipro.com/favicon.ico";
+		String destinationFile = "icon.ico";
+
+	}
+
+}
