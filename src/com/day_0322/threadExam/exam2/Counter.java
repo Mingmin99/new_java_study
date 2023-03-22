0444c6e27ce8bf4c9481d204274aca7108070cce
minyoung <alsdud11737@gmail.com>
2023-03-23 01:35:29 +0900

    [feat] 0322 thread 연습문제 2 코드

--- src/com/day_0322/threadExam/exam2/Counter.java
+++ src/com/day_0322/threadExam/exam2/Counter.java
@@ -3,14 +3,27 @@ package com.day_0322.threadExam.exam2;
 public class Counter {
 	private long count = 0;
 
-	public void add(long i) {
+	public synchronized void add(long i) {
 		System.out.println("더하기");
 		count += i;
 	}
 
-	public void mul(long i) {
+	public synchronized void mul(long i) {
 		System.out.println("곱하기");
 		count *= i;
 	}
 
+	public static void main(String[] args) throws InterruptedException {
+		Counter counter = new Counter();
+		new Thread(() -> {
+			counter.add(2);
+		}).start();
+		new Thread(() -> {
+			counter.mul(100);
+		}).start();
+
+		Thread.sleep(1000);
+		System.out.println(counter.count);
+
+	}
 }
