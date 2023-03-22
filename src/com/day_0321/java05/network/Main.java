1f5e01b92fde711106d841c736065f767b4682be
minyoung <alsdud11737@gmail.com>
2023-03-22 09:09:18 +0900

    [feat] 0321 java 응용8장 network 코드연습

--- src/com/day_0321/java05/network/Main.java
+++ src/com/day_0321/java05/network/Main.java
@@ -1,15 +1,30 @@
 package com.day_0321.java05.network;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 //		java.net.URL 클래스를 사용하여 다음 주소의 그림 파일을 읽어서 PC에 저장하는 프로그램을 작성하시오.
 //		그림 파일 주소 : https://alimipro.com/favicon.ico
 //		저장 위치 : 아무데나
 //		파일명 : icon.ico
-		String imageUrl = "https://alimipro.com/favicon.ico";
-		String destinationFile = "icon.ico";
+		BufferedInputStream in = new BufferedInputStream(new URL("https://alimipro.com/favicon.ico").openStream());
+
+		FileOutputStream fos = new FileOutputStream("icon.ico");
+		{
+
+			byte dataBuffer[] = new byte[1024];
+			int byteRead;
+
+			while ((byteRead = in.read(dataBuffer, 0, 1024)) != 1) {
+				fos.write(dataBuffer, 0, byteRead);
+			}
+		}
 
 	}
 
