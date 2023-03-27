1828814bd958ba1206cd360f159add8789c98322
minyoung <alsdud11737@gmail.com>
2023-03-28 01:54:04 +0900

    [feat] 0328 algorithm 문자열 뒤집기
    문자열 뒤집기 풀어보기

---
+++ src/algorithm/StringReversal.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class StringReversal {
+
+	class Solution {
+		public String solution(String my_string) {
+			String answer = "";
+
+			StringBuffer sb = new StringBuffer(my_string);
+			String reverse = sb.reverse().toString();
+			
+			return answer = reverse;
+			
+		}
+	}
+
+}
